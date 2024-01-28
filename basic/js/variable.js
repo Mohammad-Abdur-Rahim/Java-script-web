64d4da95b49970efac0215da146265a9b9f6c59c
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-01-28 11:08:08 +0600

    *** Variable declare done ***

---
+++ basic/js/variable.js
@@ -0,0 +1,7 @@
+var weight = 58;
+var money = 30;
+var salary = 30000;
+
+var intern =10000;
+console.log("My 2024 intern is :",intern, "Taka Alhamdulilah");
+console.log("My 2024 Salary is :",salary, "Taka Alhamdulilah");

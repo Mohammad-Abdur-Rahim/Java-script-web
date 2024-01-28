e749ba50432af44d6100d725ac0f633dd8126040
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-01-28 23:05:48 +0600

    *** Ternarie Operatore Simple use  ***

--- basic/js/condition.js
+++ basic/js/condition.js
@@ -1,17 +1,6 @@
-//Discount code implement in js 
-const TShirt_price =5000;
-if(TShirt_price>=2000 && TShirt_price<=3000){
-//10% discount code
-const discount  =TShirt_price*10/100;
-const payamount =TShirt_price-discount;
-console.log("You Receive 10% Discount :"+payamount);
-}
-else if(TShirt_price>=3000 && TShirt_price<=5000){
-    //20% discount code
-    const discount  =TShirt_price*20/100;
-    const payamount =TShirt_price-discount;
-    console.log("You Receive 20% Discount :"+payamount);
-    }
-else{
-    console.log("You Have no Discount");
-}
+//  Simple Ternarie operator Here
+var age=12;
+age>=18? console.log("Vote dio"):console.log("Vote not Allow");
+//  Simple Ternarie operator Here
+var age=22;
+age>=18? console.log("Vote dio"):console.log("Vote not Allow");

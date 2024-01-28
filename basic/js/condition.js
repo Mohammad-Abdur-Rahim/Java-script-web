364aa6d083cd0e0bd378146d83f264a081a74c92
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-01-28 22:12:27 +0600

    *** Logical || && Use ***

--- basic/js/condition.js
+++ basic/js/condition.js
@@ -1,6 +1,18 @@
-var projectPrice=12000;
-if(projectPrice >10000){
-    console.log("Yes I Work Your Project");
-}else{
-    console.log("No I not work with you");
+var salary = 33000;
+var off2day = true;
+
+if(salary > 25000 || off2day==true)   {
+console.log("Plan A I work Bangladesh with your It Company");   
+}
+else{
+    console.log("Plan B go to Germany for masters");
+}
+var Project_Price = 18000;
+var project_Work_10_Days = true;
+
+if(Project_Price > 15000 && project_Work_10_Days==false)   {
+console.log(" I work with your project");   
+}
+else{
+    console.log("Not interested work on your project Price is low");
 }

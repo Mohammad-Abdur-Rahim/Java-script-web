978429f7d1c5931cfe1abee7643a14e6e3df08ab
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-01-29 21:10:03 +0600

    *** Array Print use  ***

--- basic/js/condition.js
+++ basic/js/condition.js
@@ -1,4 +1,10 @@
 //  Array code here
 const number_list = [1,2,3,4,5,6,7,8,9,10,11];
-const Friend_list = ['abdu','babul','kalam','rasel'];
+const friend_list = ['abdu','babul','kalam','rasel'];
+const friend2 = friend_list[1]
+
+console.log(friend_list);
+console.log(friend_list[2]);
+console.log(friend2);
+
 

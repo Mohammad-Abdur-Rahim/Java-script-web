48f2973aa3af7327d6292d5bed7df3faaeebd126
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-01-30 22:53:53 +0600

    **Js Whil loop Sum **

--- basic/js/practise.js
+++ basic/js/practise.js
@@ -1,7 +1,10 @@
-let num=0;
-while(num<5){
-    console.log("loop");
-    // num=num+1;
-    num++;
+//sum of first 10 numbers 
+let num=1;
+sum=0;
+while(num<=10){
+    console.log(num);
+    sum=sum+num;
    
+    num++;
 }
+console.log("sum is:",sum);

e3f1c4b8859fc48edf225591b12ce5ea40bfb7aa
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-03 13:36:10 +0600

    *** String .trim() fn  ***

--- basic/String.js
+++ basic/String.js
@@ -1,7 +1,7 @@
-const subject='Math';
-const book = 'math'
-//Use tolower and toUpper 
-if(subject.toLowerCase() === book.toLowerCase()){
+const subject='math';
+const book = ' math'
+//Use tolower and toUpper  & Trim Fn
+if(subject.trim() === book.trim()){
     console.log('good mark');
 }else{
     console.log('Fail mark');

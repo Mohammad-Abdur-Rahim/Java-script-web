5518f2314809ea0bbdcf10f061c639e65c2f5469
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-03 14:08:31 +0600

    *** String Reverse  fn  ***

--- basic/String.js
+++ basic/String.js
@@ -1,18 +1,6 @@
-// Function .slice .split.join.concat.includes
+//String Revers 3 Way
 
-// const country="Bangladesh";
-// console.log(country.slice(6,10));
+let sentences ='i am Full-Stack Developer';
 
-const country1="Bangladesh";
-console.log(country1.split());
-
-const country3=["Bangladesh","india"];
-console.log(country3.join('|'));
-
-const fname = 'Md';
-const lname = 'Rahim';
-console.log(fname.concat(' ').concat(lname));
-
-
-
+console.log(sentences);
 

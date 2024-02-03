6aace47775df42194d3622b0850d1816a3ddebbe
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-03 15:13:17 +0600

    *** Object Dot & Bracket notaion use case  ***

--- basic/object.js
+++ basic/object.js
@@ -5,5 +5,7 @@ profession:'Developer',
 Salary:33000,
 married:false,
 }
-
+person.name = 'Full-Stack Developer Rahim';    //. Dot notaion access and value set 
+person['age']=23;               // [] Bracket notaion access and value set 
 console.log(person.name);
+console.log(person['age']);

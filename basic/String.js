d7d5b8fec342691acd32cf4225f8055d7de7b07f
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-03 13:51:03 +0600

    *** String .slice splite join concat includes etc fn  ***

--- basic/String.js
+++ basic/String.js
@@ -1,8 +1,18 @@
-const subject='math';
-const book = ' math'
-//Use tolower and toUpper  & Trim Fn
-if(subject.trim() === book.trim()){
-    console.log('good mark');
-}else{
-    console.log('Fail mark');
-}
+// Function .slice .split.join.concat.includes
+
+// const country="Bangladesh";
+// console.log(country.slice(6,10));
+
+const country1="Bangladesh";
+console.log(country1.split());
+
+const country3=["Bangladesh","india"];
+console.log(country3.join('|'));
+
+const fname = 'Md';
+const lname = 'Rahim';
+console.log(fname.concat(' ').concat(lname));
+
+
+
+

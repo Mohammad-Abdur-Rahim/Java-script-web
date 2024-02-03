06d9562bd88f47fca52773bbf65a218d0f48829f
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-03 15:21:48 +0600

    *** Object keys & Values fn  ***

--- basic/object.js
+++ basic/object.js
@@ -5,6 +5,8 @@ profession:'Developer',
 Salary:33000,
 married:false,
 }
-// object key means all propertise name in object
+// object key & Values means all propertise name & value in object
  const keys=Object.keys(person);
+ const values=Object.values(person);
  console.log(keys);
+ console.log(values);

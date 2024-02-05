ba32a60d130f839c4fa4c79a7ae31691d79a1932
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-05 13:37:49 +0600

    *** Array Reverse .unshift Fn ***

--- basic/js/reverseArray.js
+++ basic/js/reverseArray.js
@@ -1,5 +1,13 @@
-//Array Reverse Function Implements 
-const number =[1,2,3,45,6,7,889];
-console.log(number);
-number.reverse();
-console.log(number);
+
+
+//Array Reverse Function for lo0p[] unshift Implements 
+
+const numbers =[1,2,3,45,6,7,889];
+const rev_number=[];
+
+
+for(let i=0;i<numbers.length;i++){
+const num = numbers[i];
+rev_number.unshift(num);
+}
+console.log(rev_number);

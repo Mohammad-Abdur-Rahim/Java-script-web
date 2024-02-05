8481407ace6d1be5abd7b1daea010a9899b5bf41
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-05 13:53:53 +0600

    *** Array Sort Accending Decending use  Fn ***

--- basic/js/sort.js
+++ basic/js/sort.js
@@ -1,6 +1,9 @@
 // sort ing array way
 
-const numbers=[99,33,44,55,1,2,3,44];
-const numsort = numbers.sort();
+const numbers1=[99,33,44,55,1,2,3,44];
+const numbers2=[99,33,44,55,1,2,3,44];
+const numsort1 = numbers1.sort(function(a,b){return a-b}); //assending
+const numsort2 = numbers2.sort(function(a,b){return b-a}); //desending
 
-console.log(numsort);
+console.log(numsort1);
+console.log(numsort2);

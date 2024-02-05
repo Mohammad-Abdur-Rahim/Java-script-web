82ccab8f0ef29f6fd4704b04135fba298cd7322e
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-05 13:18:38 +0600

    *** Array Traverse ***

---
+++ basic/js/arraytraverse.js
@@ -0,0 +1,23 @@
+const friends =['md','rana','islam','miron'];
+
+for(const friend of friends){
+    // console.log(friend);
+}
+for (let i=0;i<friends.length;i++){
+    console.log(i);
+    console.log(friends[i]);
+} 
+//for loop use
+ 
+// const number =[1,2,3,4,5,6,7,8,9];
+// for(let i=0;i<number.length;i++){
+//     console.log(number[i]);
+// }
+
+//while loop use 
+const number =[1,2,3,4,5,6,7,8,9];
+let i=0;
+while(i<number.length){
+    console.log(number[i]);
+    i++;
+}

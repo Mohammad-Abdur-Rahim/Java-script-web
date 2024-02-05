d4edad38a561d81859769d4896934bc824504178
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-05 15:42:49 +0600

    *** Function Return value how to print  ***

---
+++ basic/js/return.js
@@ -0,0 +1,20 @@
+//Function return usecase here implements 
+
+
+function add(number){
+    const addnumber =number+number;
+    return addnumber;
+}
+function multi(number){
+    const multinumber =number*number;
+    return multinumber;
+}
+/*
+add(5);
+multi(10);
+return value dakajabe nah tai function ka varaiable a nia print korsee
+*/
+const output =add(5);
+const output2 =multi(10);
+console.log(output);
+console.log(output2);

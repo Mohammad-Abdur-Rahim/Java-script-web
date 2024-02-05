d2222bd310212a414e3ac434959d4338cc3f2358
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-05 16:09:09 +0600

    *** Function array elements sum  ***

---
+++ basic/js/arrayFunction.js
@@ -0,0 +1,21 @@
+//Array function Build here sum of array elements
+
+// const number =[1,2,3,4,5,6,7,8,9];
+// let sum=0;
+// for(let i=0;i<=number.length;i++){
+//     sum=sum+i;
+//     console.log(number[i]);
+   
+// }
+// console.log(sum);
+
+
+function arrayadd(number){
+    let sum=0;
+    for(let i=0;i<=number.length;i++){
+        sum=sum+i;
+    }
+    console.log(sum);
+}
+ const number =[1,2,3,4,5,6,7,8,9];
+arrayadd(number);

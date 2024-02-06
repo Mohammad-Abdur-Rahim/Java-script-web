e3b19e8f1155e73c9622ebbfb394080068c7b0a4
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-06 15:17:33 +0600

    ***Swap two number with temp***

---
+++ basic/js/swap.js
@@ -0,0 +1,11 @@
+//Swap Variable, Swap Without Temp, Destructing
+
+
+let a=10,b=20;
+
+console.log("Before Swap two number",a,b);
+
+let temp=a;
+a=b;
+b=temp;
+console.log("After Swap two number",a,b);

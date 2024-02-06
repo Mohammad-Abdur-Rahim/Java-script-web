91cac0918f5a81fc8781af356efcfae3804d76f6
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-06 16:29:38 +0600

    ** Find max and min in Arrays***

---
+++ basic/js/arrayBignumber.js
@@ -0,0 +1,36 @@
+// Find The Max Number In An Array
+
+
+// Find Array maximum Numbers 
+function getmax(number){
+    let max=number[0];
+for(const num of number){
+   if(num>max){
+    max=num;
+   }
+}
+return max;
+}
+
+const height=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+const mx = getmax(height);
+console.log("Find maximum numbers :",mx);
+
+
+
+
+
+// Find Array minimum Numbers 
+function getmin(number){
+    let min=number[0];
+for(const num of number){
+   if(num<min){
+    min=num;
+   }
+}
+return min;
+}
+
+const height1=[111,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+const mx1 = getmin(height1);
+console.log("Find Minimum numbers :",mx1);

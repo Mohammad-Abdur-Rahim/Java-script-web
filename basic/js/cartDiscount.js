a4ae68d2a6526abd405dea113a518fd1ed91fc3b
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-06 18:55:03 +0600

    Cart Discount Price

---
+++ basic/js/cartDiscount.js
@@ -0,0 +1,19 @@
+// (Advanced) Multi-Layer Discount Price Calculation
+
+
+function discounPrice(quantity){
+    if(quantity<=100){
+        const total=quantity*100;
+        return total;
+    }
+    else if(quantity<=200){
+        const total=quantity*90;
+        return total;
+    }
+    else{
+        const total=quantity*70;
+        return total;
+    }
+}
+const total=discounPrice(10);
+console.log(total);

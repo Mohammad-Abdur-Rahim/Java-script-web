b4c88b05bbd16f189c96312b27411cb92fba2322
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-06 14:59:58 +0600

    ***Date and Time Function***

---
+++ basic/js/dateTime.js
@@ -0,0 +1,7 @@
+//Date and Time Function Implements here 
+
+const today= new Date();
+console.log(today);
+const date =new Date('2029-09-29');
+console.log(date.getMonth());
+console.log(date.getDay());

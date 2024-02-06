2299f98781f1b1dd29c46331b824792330ec8e76
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-06 14:42:15 +0600

    ***(math max,min,Pi)  ***

---
+++ basic/js/math.js
@@ -0,0 +1,6 @@
+//Math, Abs, Round, Ceil, Floor, And Random Number
+const max=Math.max(1,2,3,4,5,6,7,8,9,222,444,566,6);
+console.log(max);
+const min=Math.min(1,2,3,4,5,6,7,8,9,222,444,566,6);
+console.log(min);
+console.log(Math.PI);

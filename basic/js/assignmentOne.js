1f44c8e1b0faa48b117d12c40119e348c45e3770
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-08 12:45:36 +0600

    *** Assignment 1 Done ***

--- basic/js/assignmentOne.js
+++ basic/js/assignmentOne.js
@@ -1,10 +1,10 @@
 // AssignMent 1 Code Is Here.........................
-function calculateMoney(ticket){
-    if (ticket < 0) {
-        return "Error.";
+function calculateMoney(ticketSale){
+    if (ticketSale < 0) {
+        return "Error.Invalid Number";
     }
 const perTicketPrice=120;
-const totalSumOfTicketPrice=ticket*perTicketPrice; 
+const totalSumOfTicketPrice=ticketSale*perTicketPrice; 
 const perDayCost=(500+(8*50));
 const dailyIncome=totalSumOfTicketPrice-perDayCost;
 return dailyIncome;

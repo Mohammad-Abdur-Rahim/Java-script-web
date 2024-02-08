4e86b8c22a8ad82381d4bdbb10636e0e3bdb8d37
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-08 13:44:08 +0600

    *** Assignment 2 Done ***

--- basic/js/assignmentTwo.js
+++ basic/js/assignmentTwo.js
@@ -1,2 +1,23 @@
 //Assignment 2 code is here
 
+function checkName(name){
+    //Unexpected handle start
+    if(typeof name!=='string'){
+        return "invalid";
+    }
+    //Unexpected handle end
+    
+    const lastChar = name.charAt(name.length - 1);
+    const nametext = ['a', 'y', 'i', 'e', 'o', 'u', 'w','A','Y','I','E','O','U','W'];
+    if (nametext.includes(lastChar)) {
+        return 'Good Name';
+    }
+    else{
+        return "Bad Name";
+
+    }
+
+}
+
+const result = checkName('Salman');
+console.log(result);

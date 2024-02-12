3dcc4e2e737c8b2f3830379046df9efade5452d6
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-12 12:47:15 +0600

    *** Object display  ***

---
+++ basic/js/object.js
@@ -0,0 +1,12 @@
+const student ={
+    name: 'md abdur Rahim',
+    age: 22,
+    issingle: true,
+    cglist:[1,2,3,4,5,6,7,8,9,10],
+    car:{
+        brand:'tesla',
+        price:200000,
+        version:2026,
+    }
+}
+console.log(student.cglist);

6a0bd1c377f635a0873ec312ae60fad94658a2af
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-12 13:34:53 +0600

    *** MatchingFunction ***

--- basic/js/matching.js
+++ basic/js/matching.js
@@ -1,15 +1,26 @@
+//Matching Function javascript here
 const products =[
-    {id:1,name:'md',salary:33000},
-    {id:2,name:'md',salary:33000},
-    {id:3,name:'md',salary:33000},
-    {id:4,name:'md',salary:33000},
-    {id:5,name:'md',salary:33000},
-    {id:6,name:'md',salary:33000},
-    {id:7,name:'md',salary:33000},
-    {id:8,name:'md',salary:33000},
+    {id:1,name:'iphone iphone',salary:33000},
+    {id:2,name:'samsung',salary:33000},
+    {id:3,name:'ximio',salary:33000},
+    {id:4,name:'redmi',salary:33000},
+    {id:5,name:'vivo',salary:33000},
+    {id:6,name:'Iphone',salary:33000},
+    {id:7,name:'realme',salary:33000},
+    {id:8,name:'software',salary:33000},
  
 
 ];
-for (const product of products) {
-    console.log(product);
+function matchingProduct(product,search){
+    const matchedProduct=[];
+    for(const product of products){
+        if(product.name.toLowerCase().includes(search.toLowerCase())){
+            matchedProduct.push(product);
+        }
+    }
+    return matchedProduct;
 }
+const result = matchingProduct(products,'Iphone');
+console.log(result);
+
+

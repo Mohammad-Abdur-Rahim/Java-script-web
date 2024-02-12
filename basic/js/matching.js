28e4dd2ebe0bc8c431e6806b892df85168732f5d
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-12 13:01:57 +0600

    *** Loop easy way for of  ***

---
+++ basic/js/matching.js
@@ -0,0 +1,11 @@
+const numbers = [12,11,22,33,44,55];
+
+// for(let i = 0; i < number.length; i++) {
+//     const numbers=number[i];
+//     console.log(numbers);
+// }
+
+
+for(const number of numbers) {
+    console.log(number);
+}

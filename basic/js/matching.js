8af7a86e97046cded2e8458cb45f9ca9cf150180
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-02-12 13:09:52 +0600

    *** Array Object loop  ***

--- basic/js/matching.js
+++ basic/js/matching.js
@@ -1,11 +1,15 @@
-const numbers = [12,11,22,33,44,55];
+const products =[
+    {id:1,name:'md',salary:33000},
+    {id:2,name:'md',salary:33000},
+    {id:3,name:'md',salary:33000},
+    {id:4,name:'md',salary:33000},
+    {id:5,name:'md',salary:33000},
+    {id:6,name:'md',salary:33000},
+    {id:7,name:'md',salary:33000},
+    {id:8,name:'md',salary:33000},
+ 
 
-// for(let i = 0; i < number.length; i++) {
-//     const numbers=number[i];
-//     console.log(numbers);
-// }
-
-
-for(const number of numbers) {
-    console.log(number);
+];
+for (const product of products) {
+    console.log(product);
 }

6c842497d1559697435766b3fa8b82d3e4d9c700
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-03-03 11:09:20 +0600

    Function Default value set

--- Es6/indesx.js
+++ Es6/indesx.js
@@ -1,1 +1,6 @@
-console.log("test");
+function add(num1,num2=0){ //default value set function
+    const result = num1 + num2;
+    console.log(num1 ,num2,result);
+    return result;
+}
+const sum = add(10);

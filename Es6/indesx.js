7f7efd0e3552b87ff311efb25bcd35681d7842bd
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-03-03 11:20:03 +0600

    Strig BackTik ** Use in js

--- Es6/indesx.js
+++ Es6/indesx.js
@@ -2,4 +2,4 @@ const num1 =10;
 const num2 =900;
 const result =num1 + num2;
 
-console.log(`the num1 ${num1} and num2 ${num2} sum is : ${result}`);
+console.log(`The num1 ${num1} and num2 ${num2} sum is : ${result}`);

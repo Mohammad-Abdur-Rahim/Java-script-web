76e92cf736f3991c57053bff2db1579daded39a6
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-03-03 20:38:11 +0600

    object array input data

--- Es6/indesx.js
+++ Es6/indesx.js
@@ -1,11 +1,7 @@
-const phone={
-    name:"iphone xs",
-    color:"White",
-    price:25000,
-    isClean:true,
-}
-console.log(Object.keys(phone));  //Object Keys find
-console.log(Object.values(phone));  //Object values find
-console.log(Object.entries(phone));  //Object entries find
-delete phone.isClean;
-console.log(Object.entries(phone));
+const product =[
+    {no:1,name:"lenovo",price:20000},
+    {no:2,name:"macbook",price:120000},
+
+];
+console.log(product[0].price);
+console.log(product[1].price);

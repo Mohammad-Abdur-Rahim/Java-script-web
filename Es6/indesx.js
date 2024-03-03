34d6fe762b79d4fa952cf05cb05c4cb2d3fe514f
Mohammad-Abdur-Rahim <abdur.rahim2992001@gmail.com>
2024-03-03 12:19:38 +0600

    Math.max function in Js ***

--- Es6/indesx.js
+++ Es6/indesx.js
@@ -1,5 +1,2 @@
-//Object to arrow Function Find Value
-const getAge =(person)=>`${person.age} ${person.name}`;
-const student={name:"Md abdur rahim" ,age:23};
-const age = getAge(student);
-console.log(age);
+const max = Math.max(10,20,30,40,5,6,7,88);
+console.log(max);
